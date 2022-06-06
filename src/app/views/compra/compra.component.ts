b456bf36b3a9e026ed52f5166ba123dacc25d60d
GuilhermeReiis <guiguibrx2@gmail.com>
2022-06-06 12:24:14 -0300

    finalizaçao compra

--- src/app/views/compra/compra.component.ts
+++ src/app/views/compra/compra.component.ts
@@ -6,8 +6,16 @@ import { Aluno } from 'src/app/interface/aluno';
 import { Curso } from 'src/app/interface/curso';
 import { MatStepper } from '@angular/material/stepper';
 import { select } from 'd3-selection';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Venda } from 'src/app/interface/venda';
+import { LocalStorageService } from 'src/app/local-storage.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compra',
@@ -19,11 +27,12 @@ export class CompraComponent implements OnInit {
   cursos: Curso[] = [];
 
   alunoSelecionado!: any;
-  cursosSelecionados: any = [];
+  cursosSelecionados: any = false;
   valorTotal = 0;
   valorPago = 0;
   troco = 0;
-  venda!: FormGroup;
+  venda: FormGroup;
+  isSubmitted = false;
 
   //Cursos
   selection = new SelectionModel<Curso>(true, []);
@@ -38,18 +47,23 @@ export class CompraComponent implements OnInit {
   ];
   clickedRows = new Set<Aluno>();
 
-  constructor(private taskServices: TaskService,
-    private fb: FormBuilder
-    ) {
-    this.venda = this.fb.group({
-      curso: [],
-      aluno: [],
-      valor: [],
-      troco: [''],
+  constructor(
+    private taskServices: TaskService,
+    private fBuilder: FormBuilder,
+    private localStorage: LocalStorageService,
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.venda = this.fBuilder.group({
+      curso: [''],
+      aluno: [''],
+      valor: [null],
+      valorPg: [null],
+      troco: [null],
       vendedor: [''],
     });
   }
-  
+
   ngOnInit(): void {
     // Get alunos
     this.taskServices.getAlunos().subscribe((res) => {
@@ -61,48 +75,58 @@ export class CompraComponent implements OnInit {
       this.cursos = res.cursos;
     });
 
-    // Formulario de envio da venda
-    this.venda = this.fb.group({
-      curso: [this.cursosSelecionados],
-      aluno: [this.alunoSelecionado],
-      valor: [this.valorTotal],
-      troco: [''],
-      vendedor: [''],
+    //Get Vendedor
+    this.venda.patchValue({
+      vendedor: JSON.parse(window.localStorage.getItem('user')!)?.user,
     });
+    console.log(this.venda);
+
+    // Formulario de envio da venda
+    // this.venda = this.fb.group({
+    //   curso: [this.cursosSelecionados],
+    //   aluno: [this.alunoSelecionado],
+    //   valor: [this.valorTotal],
+    //   valorPago:[''],
+    //   troco: [''],
+    //   vendedor: [''],
+    // });
   }
 
   // ALUNO//////////////////////
   selectAluno(teste: MatStepper, row: any) {
     this.alunoSelecionado = row;
     this.venda.get('aluno')?.setValue(row);
-    console.log(this.venda.get('aluno')?.value)
     teste.next();
+    this.venda.patchValue({
+      aluno: this.alunoSelecionado,
+    });
 
     return this.alunoSelecionado;
   }
   selectOneCourse(selection: any, row: any) {
     selection.toggle(row);
 
-    
     const initialValue = 0;
     let courseSelecteds = Array.from(selection._selection);
-    
+
     this.venda.get('curso')?.setValue(courseSelecteds);
-    console.log(this.venda.get('curso')?.value)
+
     this.valorTotal = courseSelecteds.reduce(
       (previousValue: any, currentValue: any) =>
         previousValue + currentValue.valor,
       initialValue
     ) as any;
-    console.log(this.valorTotal)
+
+    this.venda.patchValue({
+      curso: courseSelecteds,
+      valor: this.valorTotal,
+    });
   }
 
   //CURSOS//////////////
   selectCursos(row: any) {
     this.cursosSelecionados = Array.from(row._selection) as any;
     // this.valorTotal += row._selection
-    console.log(row);
-    console.log(this.cursosSelecionados);
     return this.cursosSelecionados;
   }
 
@@ -131,8 +155,39 @@ export class CompraComponent implements OnInit {
   }
 
   somaTroco() {
-    console.log(this.valorTotal);
-    this.troco = -(this.valorTotal - this.valorPago);
+    this.venda.patchValue({
+      troco: this.venda.value.valorPg - this.valorTotal,
+    });
   }
 
+  sendSell() {
+    this.isSubmitted = true;
+
+    this.taskServices.addVenda(this.venda.value).subscribe(
+      (res) => {
+        this._snackBar.open('Venda finalizada com sucesso!!', 'Fechar', {
+          duration: 2000,
+        });
+        console.log(res);
+      },
+      (err) => {
+        this._snackBar.open('Erro ao finalizar venda!', 'Fechar', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+
+  disable() {
+    if (
+      !parseFloat(this.venda.get('valorPg')?.value!) ||
+      !parseFloat(this.valorTotal.toString())
+    ) {
+      return true;
+    }
+    return !(
+      parseFloat(this.venda.get('valorPg')?.value!) >=
+      parseFloat(this.valorTotal.toString())
+    );
+  }
 }
